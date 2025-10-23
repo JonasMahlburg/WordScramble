6bc774e6652368dd9c8645c355882db1a9af573f
JonasMahlburg <darrylmanga@yahoo.com>
2025-10-23 07:59:51 +0200

    Initial Commit

---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Jonas Mahlburg on 23.10.25.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
